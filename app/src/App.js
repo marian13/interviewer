3b4246971f42c3bc8924df298de9fc54ea2aca38
Marian13 <mariankostyk13895@gmail.com>
2022-03-08 16:21:54 +0200

    feat(split questions): split questions by topics

--- app/src/App.js
+++ app/src/App.js
@@ -4,14 +4,26 @@ import Row from 'react-bootstrap/Row';
 import Questions from './questions';
 
 import sortBy from 'lodash/sortBy';
+import slice from 'lodash/slice';
+
+const SortedQuestions = sortBy(Questions, 'number');
+
+const RubyCoreQuestions = slice(SortedQuestions, 0, 22);
+const RailsQuestions = slice(SortedQuestions, 22);
+
+const renderQuestions = ({ Questions }) => Questions.map(Question => <Question key={Question.displayName} />);
 
 const App = () => (
   <Container>
     <Row className="mx-0">
       <Col xs={6}>
-        <h1>Ruby</h1>
+        <h1>Ruby Core</h1>
 
-        {sortBy(Questions, 'number').map(Question => <Question key={Question.displayName} />)}
+        {renderQuestions({ Questions: RubyCoreQuestions })}
+
+        <h1>Rails</h1>
+
+        {renderQuestions({ Questions: RailsQuestions })}
       </Col>
     </Row>
   </Container>
